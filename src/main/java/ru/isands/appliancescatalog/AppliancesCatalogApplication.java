f04f517e19ad6a369ee1ba9ee57d91339ee267df
eduard <eduard42rus@mail.ru>
2024-01-24 12:44:20 +0700

    init commit

---
+++ src/main/java/ru/isands/appliancescatalog/AppliancesCatalogApplication.java
@@ -0,0 +1,13 @@
+package ru.isands.appliancescatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppliancesCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppliancesCatalogApplication.class, args);
+	}
+
+}
